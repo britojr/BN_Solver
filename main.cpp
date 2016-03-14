6d6667a8d4758800cf29a6cd2a6698aaf44acc55
NonWhite <wperezurcia@gmail.com>
2016-03-13 22:54:16 -0300

    added a variable string for help messages

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
  *
  * Created on 6 de diciembre de 2015, 10:32 AM
  */
-
 #include <cstdlib>
 #include <ctime>
 #include <math.h>
@@ -35,105 +34,76 @@
 
 namespace po = boost::program_options ;
 
-/**
- * The file containing the data.
- */
+/* The file containing the data */
 std::string inputFile ;
+std::string inputFileString = "The data set file. First positional argument." ;
 
-/**
- * The file to write the scores.
- */
+/* The file to write the scores */
 std::string outputFile ;
+std::string outputFileString = "The score file. Second positional argument." ;
 
-/**
- * The delimiter in each line of data file.
- */
+/* The delimiter in each line of data file */
 char delimiter = ',' ;
+std::string delimiterString = "The delimiter of the input file." ;
 
-/**
- * The minimum number of records in the AD-tree.
- */
+/* The minimum number of records in the AD-tree */
 int rMin = 5 ;
+std::string rMinString = "The minimum number of records in the AD-tree nodes." ;
 
-/**
- * A hard limit on the size of parent sets.
- */
+/* A hard limit on the size of parent sets */
 int maxParents = -1 ;
+std::string maxParentsString = "The maximum number of parents for any variable. A value less than 1 means no limit." ;
 
-/**
- * The number of threads to use.
- */
+/* The number of threads to use */
 int threadCount = 1 ;
+std::string threadString = "The number of separate threads to use for score calculations." ;
 
-/**
- * The maximum amount of time to use for each variable.
- */
+/* The maximum amount of time to use for each variable */
 int runningTime = -1 ;
+std::string runningTimeString = "The maximum amount of time to use for each variable. A value less than 1 means no limit." ;
 
-/**
- * Whether the data file has variable names in the first row.
- */
+/* Whether the data file has variable names in the first row */
 bool hasHeader = false ;
+std::string hasHeaderString = "Add this flag if the first line of the input file gives the variable names." ;
 
-/**
- * Whether to prune the scores before printing
- */
+/* Whether to prune the scores before printing */
 bool prune = true ;
+std::string pruneString = "Add this flag if the scores should NOT be pruned at the end of the search." ;
 
-/**
- * Whether to use deCampos-style pruning.
- */
+/* Whether to use deCampos-style pruning */
 bool enableDeCamposPruning = true ;
+std::string deCamposPruningString = "Add this flag if the scores should be pruned while calculating." ;
 
-/**
- * The data structure to use to calculate best parent set scores.
- * "tree", "list", "bitwise"
- */
+/* The data structure to use to calculate best parent set scores */
 std::string bestScoreCalculator ;
 
-/**
- * The type of initializer to use.
- */
+/* The type of initializer to use */
 std::string initializerType ;
 
-/**
- * Number of solutions to be generated with initializer.
- */
+/* Number of solutions to be generated with initializer */
 int numSolutions = 1 ;
+std::string numSolutionsString = "Number of initial solutions to be generated." ;
 
-/**
- * Number of iterations for greedy search until stopping
- */
+/* Number of iterations for greedy search until stopping */
 int maxIterations = 500 ;
+std::string maxIterationsString = "Max number of iterations in greedy search." ;
 
-/*
- * Method to select parent sets
- */
+/* Method to select parent sets */
 std::string selectionType = "sequential" ;
 
-/**
- * File specifying constraints on the scores.
- */
+/* File specifying constraints on the scores */
 std::string constraintsFile ;
 
-/**
- * The scoring function to use.
- */
+/* The scoring function to use */
 std::string sf = "BIC" ;
 
-/**
- * A reference to the scoring function object.
- */
+/* A reference to the scoring function object */
 scoring::ScoringFunction *scoringFunction ;
 
-/**
- * The network information.
- */
+/* The network information */
 datastructures::BayesianNetwork network ;
 
-/**
- * The constraints information
- */
+/* The constraints information */
 scoring::Constraints *constraints ;
 
 inline std::string getTime(){
@@ -204,9 +174,8 @@ void calculateScore(){
 		threads.push_back( workerThread ) ;
 	}
 
-	for( auto it = threads.begin() ; it != threads.end() ; it++){
+	for( auto it = threads.begin() ; it != threads.end() ; it++)
 		( *it )->join() ;
-	}
 
 	// concatenate all of the files together
 	std::map<std::string,std::string> metadata ;
@@ -244,21 +213,21 @@ int main( int argc , char** argv ){
 	po::options_description desc( description ) ;
 
 	desc.add_options()
-		( "input" , po::value<std::string > (&inputFile)->required(), "The data set file. First positional argument." )
-			( "output" , po::value<std::string > (&outputFile)->required(), "The score file. Second positional argument." )
-			( "delimiter,d" , po::value<char> (&delimiter)->required()->default_value(','), "The delimiter of the input file." )
-			( "rMin,m" , po::value<int> (&rMin)->default_value(5), "The minimum number of records in the AD-tree nodes." )
-			( "maxParents,p" , po::value<int> (&maxParents)->default_value(0), "The maximum number of parents for any variable. A value less than 1 means no limit." )
-			( "threads,t" , po::value<int> (&threadCount)->default_value(1), "The number of separate threads to use for score calculations." )
-			( "time,r" , po::value<int> (&runningTime)->default_value(-1), "The maximum amount of time to use for each variable. A value less than 1 means no limit." )
-			( "hasHeader,s" , "Add this flag if the first line of the input file gives the variable names." )
-			( "doNotPrune,o" , "Add this flag if the scores should NOT be pruned at the end of the search." )
-			( "enableDeCamposPruning,c" , "Add this flag if the scores should be pruned while calculating." )
+		( "input" , po::value<std::string > (&inputFile)->required(), inputFileString.c_str() )
+			( "output" , po::value<std::string > (&outputFile)->required(), outputFile.c_str() )
+			( "delimiter,d" , po::value<char> (&delimiter)->required()->default_value(','), delimiterString.c_str() )
+			( "rMin,m" , po::value<int> (&rMin)->default_value(5), rMinString.c_str() )
+			( "maxParents,p" , po::value<int> (&maxParents)->default_value(0), maxParentsString.c_str() )
+			( "threads,t" , po::value<int> (&threadCount)->default_value(1), threadString.c_str() )
+			( "time,r" , po::value<int> (&runningTime)->default_value(-1), runningTimeString.c_str() )
+			( "hasHeader,s" , hasHeaderString.c_str() )
+			( "doNotPrune,o" , pruneString.c_str() )
+			( "enableDeCamposPruning,c" , deCamposPruningString.c_str() )
 			( "parentSelectionType,q" ,po::value<std::string>(&selectionType)->default_value("sequential"), parentselection::parentSetSelectionString.c_str() )
 			( "bestScore,b" , po::value<std::string > (&bestScoreCalculator)->default_value( "list") , bestscorecalculators::bestScoreCalculatorString.c_str() )
 			( "initializer,z" , po::value<std::string > (&initializerType)->default_value("random"), initializers::initializerTypeString.c_str() )
-			( "numSolutions,n" , po::value<int> (&numSolutions)->default_value(1), "Number of initial solutions to be generated." )
-			( "maxIterations,k" , po::value<int> (&maxIterations)->default_value(100), "Max number of iterations in greedy search." )
+			( "numSolutions,n" , po::value<int> (&numSolutions)->default_value(1), numSolutionsString.c_str() )
+			( "maxIterations,k" , po::value<int> (&maxIterations)->default_value(100), maxIterationsString.c_str() )
 			( "help,h" , "Show this help message." )
 			;
 
