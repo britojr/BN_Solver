4c228abb383e7c2c610d17f15393de85cceac2b8
nonwhite <wperezurcia@gmail.com>
2016-10-20 19:07:51 -0200

    added beam_search

--- CLI/structure_optimizer_creator.h
+++ CLI/structure_optimizer_creator.h
@@ -14,9 +14,9 @@
 #include "acyclic_selection.h"
 #include "simulated_annealing.h"
 #include "tabu_search.h"
+#include "beam_search.h"
 
 namespace structureoptimizer {
-
 	static std::string structureOptimizerString = "The method for structure learning. [\"greedy_search\", \"acyclic_selection\"]";
 
 	inline StructureOptimizer* create( std::string type , initializers::Initializer* initializer ,
@@ -31,10 +31,12 @@ namespace structureoptimizer {
 			sto = new structureoptimizer::SimulatedAnnealing( initializer , bestScoreCalculators , structureParameterFile ) ;
 		}else if( type == "tabu_search" ){
 			sto = new structureoptimizer::TabuSearch( initializer , bestScoreCalculators , structureParameterFile ) ;
+		}else if( type == "beam_search" ){
+			sto = new structureoptimizer::BeamSearch( initializer , bestScoreCalculators , structureParameterFile ) ;
 		}else{
 			throw std::runtime_error( "Invalid algorithm selection: '" + type + 
 					"'.  Valid options are 'greedy_search', " +
-					"'acyclic_selection', 'simulated_annealing' and 'tabu_search'" ) ;
+					"'acyclic_selection', 'simulated_annealing', 'tabu_search' and 'beam_search" ) ;
 		}
 		return sto ;
 	}
