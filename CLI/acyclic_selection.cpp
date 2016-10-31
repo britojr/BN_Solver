b1c4369f5cc68abb7ecb7d3de123bce331d1a279
nonwhite <wperezurcia@gmail.com>
2016-10-31 17:26:14 -0200

    deleted unnecessary comments

--- CLI/acyclic_selection.cpp
+++ CLI/acyclic_selection.cpp
@@ -44,8 +44,8 @@ void structureoptimizer::AcyclicSelection::printParameters(){
 void structureoptimizer::AcyclicSelection::initialize(){
 	printf( " ======== Acyclic Selection ======== \n" ) ;
 	current = initializer->generate( structureoptimizer::ACYCLIC_BEHAVIOR_SET ) ;
-	printf(" === Iteration #%d ===\n" , 0 ) ;
-	current->print() ;
+	printf(" === Iteration %d ===\n" , 0 ) ;
+	current->print( true ) ;
 }
 
 datastructures::BNStructure structureoptimizer::AcyclicSelection::search_internal(){
@@ -61,8 +61,7 @@ datastructures::BNStructure structureoptimizer::AcyclicSelection::search_interna
 	}
 	printf("Iterations = %d\n" , numIterations ) ;
 	t->cancel() ;
-	// TODO: Check this, add a method to return a BNStructure in PermutationSet
-	return datastructures::BNStructure( current , bestScoreCalculators ) ;
+	return *(current->getStructure()) ;
 }
 
 structureoptimizer::PermutationSet* structureoptimizer::AcyclicSelection::findBestNeighbour( structureoptimizer::PermutationSet* set ){
@@ -80,68 +79,3 @@ structureoptimizer::PermutationSet* structureoptimizer::AcyclicSelection::doSwap
 	newSet->swap( index , index + 1 ) ;
 	return newSet ;
 }
-
-//datastructures::BNStructure structureoptimizer::AcyclicSelection::searchEfficient(){
-//	datastructures::BNStructure best ;
-//	m = std::vector<varset>( variableCount , VARSET( variableCount ) ) ;
-//	todo = std::vector<varset>( variableCount , VARSET( variableCount ) ) ;
-//	partial_bn = datastructures::BNStructure( variableCount ) ;
-//
-//	for(int j = variableCount - 1 ; j >= 0 ; j--){
-//		// Extract variable at position j
-//		int v_j = (*initial)[ j ] ;
-//
-//		// (a1) Pick best parent set with no descendants of V_j
-//		float score = bestScoreCalculators[ v_j ]->getScore( VARSET_NOT( m[ v_j ] ) ) ;
-//		varset parents = bestScoreCalculators[ v_j ]->getParents() ;
-//		partial_bn.setParents( v_j , parents , score ) ;
-//
-//		// (a2) Update matrix representation and get ancestors of V_j
-//		VARSET_NEW( ancestors , variableCount ) ;
-//		for(int i = 0 ; i < variableCount  ; i++){
-//			if( VARSET_GET( parents , i ) )
-//				VARSET_SET( m[ i ] , v_j ) ;
-//			if( VARSET_GET( m[ i ] , v_j ) )
-//				VARSET_SET( ancestors , i ) ;
-//		}
-//
-//		// (b1) Build a TODO list with descendants of V_j
-//		todo[ v_j ] = m[ v_j ] ;
-//
-//		// (b2) Start an empty TODO list to all ancestors of V_j
-//		VARSET_OR( ancestors , parents ) ;
-//		for(int i = 0 ; i < variableCount ; i++)
-//			if( VARSET_GET( ancestors , i ) )
-//				todo[ i ] = VARSET( variableCount ) ;
-//
-//		// (c) Start TODO lists of the parents of V_j with descendants of V_j
-//		for(int i = 0 ; i < variableCount ; i++)
-//			if( VARSET_GET( parents , i ) )
-//				todo[ i ] = m[ v_j ] ;
-//
-//		// (d) For each ancestor X of V_j
-//	}
-//	return best ;
-//}
-
-//void structureoptimizer::AcyclicSelection::visit( int x ){
-//	for(int i = 0 ; i < todo[ x ].size() ; i++){
-//		int y = todo[ x ][ i ] ;
-//		// if m(X, Y ) is true, then ignore Y and move on;
-//		if( VARSET_GET( m[ x ] , y ) ) continue ;
-//		// otherwise set m(X, Y ) to true
-//		VARSET_SET( m[ x ] , y ) ;
-//		varset parents = partial_bn[ x ]->getParents() ;
-//		// add Y to the todo of parents of X.
-//		for(int j = 0 ; j < variableCount ; j++)
-//			if( VARSET_GET( parents , j ) ){
-//				bool in_todo = false ;
-//				for(int k = 0 ; k < todo[ j ].size() ; k++)
-//					if( todo[ j ][ k ] == y ){
-//						in_todo = true ;
-//						break ;
-//					}
-//				if( !in_todo ) todo[ j ].push_back( y ) ;
-//			}
-//	}
-//}
