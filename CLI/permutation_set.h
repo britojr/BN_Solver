397aee481deedc0f685bb318cd41217557235fd6
nonwhite <wperezurcia@gmail.com>
2016-10-31 17:27:31 -0200

    added virtual method getStructure

--- CLI/permutation_set.h
+++ CLI/permutation_set.h
@@ -35,7 +35,7 @@ namespace structureoptimizer {
 
 			void setPermutation( std::vector<int> permutation ) ;
 			std::vector<int> getPermutation() ;
-
+			
 			int size() const ;
 
 			varset getVarset( int index ) ;
@@ -45,17 +45,18 @@ namespace structureoptimizer {
 			// Abstract methods
 			virtual void print( bool printPermutation = false ) = 0 ;
 			virtual PermutationSet* clone() = 0 ;
-//			virtual datastructures::BNStructure* getStructure() = 0 ; // TODO: Implement and test this
+			virtual datastructures::BNStructure* getStructure() = 0 ;
 
 		protected :
 			void setScore( float score ) ;
-			
+
 			// Abstract methods
 			virtual void updateScore( int adjacentPos = -1 ) = 0 ;
 
 			std::vector<int> permutation ;
 			std::vector<bestscorecalculators::BestScoreCalculator*> bestScoreCalculator ;
 			float score ;
+			datastructures::BNStructure* structure ;
 	} ;
 }
 
